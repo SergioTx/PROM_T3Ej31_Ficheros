5b66533d975fac05174592c78cda530c83bb54d1
SergioTx <txitxo37@gmail.com>
2016-11-23 09:31:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sergio/prom_t3ej31_ficheros/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.sergio.prom_t3ej31_ficheros;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1 = (Button) findViewById(R.id.btn_ex1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Buajaja");
+                Intent i = new Intent(MainActivity.this, Activity1.class);
+                startActivity(i);
+            }
+        });
+
+        Button exit = (Button) findViewById(R.id.btn_exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
